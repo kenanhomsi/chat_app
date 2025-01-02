c7e1d9855295762ce0a31191fec6107f7b838a15
kenanOrg <kenanhomsi0959@gmail.com>
2025-01-02 13:37:35 +0300

    socket url change

--- frontend/src/context/SocketContext.tsx
+++ frontend/src/context/SocketContext.tsx
@@ -21,7 +21,7 @@ export const SocketContextProvider = ({ children }: { children: React.ReactNode 
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:8000", {
+            const socket = io("https://chat-app-gph2.onrender.com/", {
                 query: {
                     userId: authUser.id,
                 },
